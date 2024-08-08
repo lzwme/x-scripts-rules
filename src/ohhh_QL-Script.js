d2123d6f9fa93bb1a949b7ba9d25fbf71c7aa953
renxia <lzwy0820@qq.com>
2024-08-08 12:30:18 +0000

    wip: 优化部分 ck 获取规则

--- src/ohhh_QL-Script.js
+++ src/ohhh_QL-Script.js
@@ -1,11 +1,3 @@
-/*
- * @Author: renxia
- * @Date: 2024-07-22 10:49:34
- * @LastEditors: renxia
- * @LastEditTime: 2024-07-25 22:52:15
- * @Description:
- */
-
 /**
  * yang7758258/ohhh_QL-Script
  * @type {import('@lzwme/whistle.x-scripts').RuleItem[]}
@@ -46,7 +38,7 @@ module.exports = [
     desc: '义乌小商品城会员-小程序签到',
     url: 'https://apiserver.chinagoods.com/ums/authentication/v1/userauthinfo',
     method: 'get',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.userinfo?.userId }),
+    getCacheUid: ({ resBody: R, headers }) => headers.authorization && { uid: R?.data?.userinfo?.userId },
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.authorization}&${d.uid}`).join('\n'), sep: '\n' } }),
     updateEnvValue: /&([\d\*]+)/,
   },
@@ -56,7 +48,7 @@ module.exports = [
     desc: 'zippo会员中心-小程序签到',
     url: 'https://wx-center.zippo.com.cn/api/users/profile',
     method: 'get',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.phone }),
+    getCacheUid: ({ resBody: R, headers }) => headers.authorization && { uid: R?.phone },
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.authorization}&${d.uid}`).join('\n'), sep: '\n' } }),
     updateEnvValue: /&([\d\*]+)/,
   },
@@ -76,7 +68,7 @@ module.exports = [
     desc: '兴攀农场-小程序',
     url: 'https://p.xpfarm.cn/treemp/user.PersonalCenter/getInfo',
     method: 'post',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.id }),
+    getCacheUid: ({ resBody: R, headers }) => headers.authorization && ({ uid: R?.data?.id }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.authorization}&${d.uid}`).join('\n') } }),
     updateEnvValue: /&([\d\*]+)/,
   },
@@ -86,7 +78,7 @@ module.exports = [
     desc: '东方棘市-小程序',
     url: 'https://ys.shajixueyuan.com/api/user/info',
     method: 'GET',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.id }),
+    getCacheUid: ({ resBody: R, headers }) => headers.token && ({ uid: R?.data?.id }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.token}#${d.uid}`).join('\n') } }),
     updateEnvValue: /#([\d\*]+)/,
   },
@@ -96,7 +88,7 @@ module.exports = [
     desc: '日清体验馆-小程序',
     url: 'https://prod-api.nissinfoodium.com.cn/gw-shop/app/v1/user-center/detail?type=1',
     method: 'GET',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.user_id }),
+    getCacheUid: ({ resBody: R, headers }) => headers.token && ({ uid: R?.data?.user_id }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.token}&${d.uid}`).join('\n') } }),
     updateEnvValue: /&([\d\*]+)/,
   },
@@ -106,7 +98,7 @@ module.exports = [
     desc: 'EMS邮惠中心-小程序签到得优惠券',
     url: 'https://ump.ems.com.cn/memberCenterApiV2/member/findByOpenIdAppId',
     method: 'post',
-    getCacheUid: ({ reqBody: Q, resBody: R }) => ({ uid: R?.data?.user_id, data: `${Q.openId}&${R.data?.user_id}` }),
+    getCacheUid: ({ reqBody: Q, resBody: R }) => Q.openId && ({ uid: R?.data?.user_id, data: `${Q.openId}&${R.data?.user_id}` }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => d.data).join('\n') } }),
     updateEnvValue: /&([\da-z]+)/,
   },
@@ -122,16 +114,6 @@ module.exports = [
     },
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => d.data).join('\n') } }),
     updateEnvValue: /&([\da-z]+)/,
-  },
-
-  {
-    on: 'req-body',
-    ruleId: 'chmlck',
-    desc: '长虹美菱小程序签到',
-    url: 'https://hongke.changhong.com/gw/burying/burying/report*',
-    method: 'post',
-    getCacheUid: ({ reqBody: R }) => R?.props?.wx_user?.phone_number,
-    handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.token}##${d.uid}`).join('\n') } }),
   },
   {
     on: 'res-body',
@@ -178,7 +160,7 @@ module.exports = [
     desc: '国乐酱酒-小程序签到得酒',
     url: 'https://member.guoyuejiu.com/api/user/info',
     method: 'get',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.id }),
+    getCacheUid: ({ resBody: R , headers }) => headers.authorization && ({ uid: R?.data?.id }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.authorization}&${d.uid}`).join('\n'), sep: '\n' } }),
     updateEnvValue: /&([\d\*]+)/,
   },
@@ -188,7 +170,7 @@ module.exports = [
     desc: 'Babaycare-小程序签到',
     url: 'https://api.bckid.com.cn/lightpay/front/user/balance/info',
     method: 'post',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.body?.userId }),
+    getCacheUid: ({ resBody: R , headers }) => headers.authorization && ({ uid: R?.body?.userId }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.authorization}&${d.uid}`).join('\n'), sep: '\n' } }),
     updateEnvValue: /&([\d\*]+)/,
   },
@@ -198,7 +180,7 @@ module.exports = [
     desc: '毛铺草本荟-小程序签到',
     url: 'https://mpb.jingjiu.com/proxy-he/api/BlzAppletIndex/levelPowerList',
     method: 'post',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.user?.user_id }),
+    getCacheUid: ({ resBody: R, headers }) => headers.authorization && ({ uid: R?.data?.user?.user_id }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers.authorization}&${d.uid}`).join('\n'), sep: '\n' } }),
     updateEnvValue: /&([\d\*]+)/,
   },
@@ -220,7 +202,7 @@ module.exports = [
     desc: '海天美味馆-小程序签到',
     url: 'https://cmallapi.haday.cn/buyer-api/members',
     method: 'get',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.member_id }),
+    getCacheUid: ({ resBody: R, headers }) => headers.authorization && ({ uid: R?.member_id }),
     handler: ({ cacheData: D }) => ({
       envConfig: { value: D.map(d => `${d.headers.authorization}&${d.headers.uuid}&${d.uid}`).join('\n'), sep: '\n' },
     }),
@@ -232,7 +214,7 @@ module.exports = [
     desc: '格力高-小程序签到',
     url: 'https://crm.glico.cn/miniapp/member/profile',
     method: 'get',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.id }),
+    getCacheUid: ({ resBody: R, headers }) => headers['x-auth-token'] && ({ uid: R?.data?.id }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.headers['x-auth-token']}&${d.uid}`).join('\n'), sep: '\n' } }),
     updateEnvValue: /&(\d+)$/,
   },
@@ -242,7 +224,7 @@ module.exports = [
     desc: '富士instax玩拍由我俱乐部-小程序签到',
     url: 'https://instax.app.xcxd.net.cn/api/me',
     method: 'get',
-    getCacheUid: ({ resBody: R }) => ({ uid: R?.data?.user_id || R?.data?.user?.id }),
+    getCacheUid: ({ resBody: R, headers }) => headers.authorization && ({ uid: R?.data?.user_id || R?.data?.user?.id }),
     handler: ({ cacheData: D }) => ({ envConfig: { value: D.map(d => `${d.uid}&${d.headers.authorization}`).join('\n'), sep: '\n' } }),
     updateEnvValue: /(\d+)&/,
   },
